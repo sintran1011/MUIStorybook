cd1b1a7786210cb5badb8b17b637a103d2947880
SinTran <sintran1011@gmail.com>
2025-01-16 19:35:17 +0700

    save code

--- src/stories/Layout/Layout.stories.tsx
+++ src/stories/Layout/Layout.stories.tsx
@@ -5,6 +5,8 @@ import Input from "@stories/Input";
 import { Search } from "@mui/icons-material";
 import { useState } from "react";
 import Button from "@stories/Button";
+import Avatar from "@stories/Avatar";
+import moment from "moment";
 
 const meta: Meta<typeof Layout> = {
   title: "Quoxent/Layout",
@@ -68,7 +70,7 @@ export const DashboardLayout = () => {
   );
 };
 
-DashboardLayout.parameter = {
+DashboardLayout.parameters = {
   docs: {
     source: {
       code: `
@@ -124,6 +126,66 @@ import Search from 'src/assets/images/icons/search.svg';
         <div className="h-[500px] w-full" />
       </Stack>
     </Box>
+      `,
+    },
+  },
+};
+
+export const UserInfo = () => {
+  return (
+    <Stack flexDirection={"row"} gap={2} alignItems={"center"}>
+      <Avatar sx={{ width: 56, height: 56 }} src={"https://picsum.photos/56"} />
+      <Stack gap={"4px"}>
+        <Typography
+          color="white"
+          fontSize={"16px"}
+          fontWeight={700}
+          lineHeight={"20px"}
+          variant="label-xl"
+        >
+          Sinbad
+        </Typography>
+        <Typography
+          color="dark.300"
+          fontSize={"14px"}
+          fontWeight={450}
+          lineHeight={"20px"}
+          variant="body-medium"
+        >
+          {moment().format("MM/DD/YYYY")}
+        </Typography>
+      </Stack>
+    </Stack>
+  );
+};
+
+UserInfo.parameters = {
+  docs: {
+    source: {
+      code: `
+       <Stack flexDirection={"row"} gap={2} alignItems={"center"}>
+      <Avatar sx={{ width: 56, height: 56 }} src={"https://picsum.photos/56"} />
+      <Stack gap={"4px"}>
+        <Typography
+          color="white"
+          fontSize={"16px"}
+          fontWeight={700}
+          lineHeight={"20px"}
+          variant="label-xl"
+        >
+          Sinbad
+        </Typography>
+        <Typography
+          color="dark.300"
+          fontSize={"14px"}
+          fontWeight={450}
+          lineHeight={"20px"}
+          variant="body-medium"
+        >
+          {moment().format("MM/DD/YYYY")}
+        </Typography>
+      </Stack>
+    </Stack>
       `,
     },
   },
