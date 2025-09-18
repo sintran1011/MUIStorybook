cff559a06df37cc2a4a01230e1599fea2fde7298
tam-tran-vns <tam.tran@vnsilicon.net>
2025-09-18 17:31:19 +0700

    test: text merge

--- src/stories/Table/index.tsx
+++ src/stories/Table/index.tsx
@@ -10,11 +10,11 @@ import {
   TableRow,
   Typography,
   TypographyProps,
-} from '@mui/material';
-import { styled } from '@mui/material/styles';
-import { ReactNode } from 'react';
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { ReactNode } from "react";
 
-import TablePaginationActions from './TablePagination';
+import TablePaginationActions from "./TablePagination";
 
 export interface GridColDef<T = Record<string, unknown>> {
   field: string;
@@ -22,7 +22,7 @@ export interface GridColDef<T = Record<string, unknown>> {
   width?: number | string;
   description?: string;
   renderCell?: (value: any, row: T) => React.ReactNode;
-  textAlign?: 'left' | 'right' | 'center';
+  textAlign?: "left" | "right" | "center";
   renderLoading?: () => ReactNode;
 }
 
@@ -39,40 +39,63 @@ export interface TableProps<T = Record<string, unknown>> {
 
 const StyledTableCell = styled(TableCell)({
   borderBottom: 0,
-  ':first-of-type': {
-    borderTopLeftRadius: '8px',
-    borderBottomLeftRadius: '8px',
-    paddingLeft: '24px',
+  ":first-of-type": {
+    borderTopLeftRadius: "8px",
+    borderBottomLeftRadius: "8px",
+    paddingLeft: "24px",
   },
-  ':last-of-type': {
-    borderTopRightRadius: '8px',
-    borderBottomRightRadius: '8px',
-    paddingRight: '24px',
+  ":last-of-type": {
+    borderTopRightRadius: "8px",
+    borderBottomRightRadius: "8px",
+    paddingRight: "24px",
   },
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
+  overflow: "hidden",
+  textOverflow: "ellipsis",
 });
 
-const renderTypography = ({ sx, text }: { sx?: TypographyProps; text: string }) => (
+const renderTypography = ({
+  sx,
+  text,
+}: {
+  sx?: TypographyProps;
+  text: string;
+}) => (
   <Typography
-    variant='body-medium'
-    fontSize={'14px'}
-    lineHeight={'20px'}
+    variant="body-medium"
+    fontSize={"14px"}
+    lineHeight={"20px"}
     sx={{
       ...sx,
-    }}>
+    }}
+  >
     {text ? text : null}
   </Typography>
 );
 
-const BasicTable = <T extends Record<string, unknown>>(props: TableProps<T>) => {
-  const { columns, data, total = 0, page = 0, size = 10, onChangePage, onChangePageSize, loading = false } = props;
+const BasicTable = <T extends Record<string, unknown>>(
+  props: TableProps<T>
+) => {
+  const {
+    columns,
+    data,
+    total = 0,
+    page = 0,
+    size = 10,
+    onChangePage,
+    onChangePageSize,
+    loading = false,
+  } = props;
 
-  const handleChangePage = (_event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+  const handleChangePage = (
+    _event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
     onChangePage(newPage);
   };
 
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     onChangePageSize(parseInt(event.target.value, 10));
     onChangePage(0);
   };
@@ -81,24 +104,38 @@ const BasicTable = <T extends Record<string, unknown>>(props: TableProps<T>) => 
     new Array(10).fill(1).map((i, idx: number) => (
       <TableRow
         sx={{
-          '&:nth-of-type(even)': {
-            backgroundColor: '#ffffff03',
+          "&:nth-of-type(even)": {
+            backgroundColor: "#ffffff03",
           },
-          '& td': {
-            padding: '8px',
+          "& td": {
+            padding: "8px",
           },
-          height: '56px',
+          height: "56px",
         }}
-        key={idx}>
-        {columns.map(col => {
+        key={idx}
+      >
+        {columns.map((col) => {
           const key: string = col.field;
           const loadSkeleton = col.renderLoading;
           return (
-            <StyledTableCell sx={{ width: col.width, maxWidth: col.width, textAlign: col.textAlign }} key={key}>
+            <StyledTableCell
+              sx={{
+                width: col.width,
+                maxWidth: col.width,
+                textAlign: col.textAlign,
+              }}
+              key={key}
+            >
               {loadSkeleton ? (
                 loadSkeleton()
               ) : (
-                <Skeleton variant='text' sx={{ fontSize: '24px', backgroundColor: 'rgba(255, 255, 255, 0.1)' }} />
+                <Skeleton
+                  variant="text"
+                  sx={{
+                    fontSize: "24px",
+                    backgroundColor: "rgba(255, 255, 255, 0.1)",
+                  }}
+                />
               )}
             </StyledTableCell>
           );
@@ -110,23 +147,31 @@ const BasicTable = <T extends Record<string, unknown>>(props: TableProps<T>) => 
     (data || []).map((row, idx: number) => (
       <TableRow
         sx={{
-          '&:nth-of-type(even)': {
-            backgroundColor: '#ffffff03',
+          "&:nth-of-type(even)": {
+            backgroundColor: "#ffffff03",
           },
-          '& td': {
-            padding: '8px',
+          "& td": {
+            padding: "8px",
           },
-          height: '56px',
+          height: "56px",
         }}
-        key={idx}>
-        {columns.map(col => {
+        key={idx}
+      >
+        {columns.map((col) => {
           const key: string = col.field;
           return (
-            <StyledTableCell sx={{ width: col.width, maxWidth: col.width, textAlign: col.textAlign }} key={key}>
+            <StyledTableCell
+              sx={{
+                width: col.width,
+                maxWidth: col.width,
+                textAlign: col.textAlign,
+              }}
+              key={key}
+            >
               {col.renderCell
                 ? col.renderCell(row[key], row)
                 : renderTypography({
-                    sx: { color: '#ffffff' },
+                    sx: { color: "#ffffff" },
                     text: row[key] as string,
                   })}
             </StyledTableCell>
@@ -137,25 +182,40 @@ const BasicTable = <T extends Record<string, unknown>>(props: TableProps<T>) => 
 
   return (
     <TableContainer>
-      <Table sx={{ minWidth: 700, '& .MuiTableCell-head': { lineHeight: '28px' } }} aria-label='customized table'>
+      <Table
+        sx={{ minWidth: 700, "& .MuiTableCell-head": { lineHeight: "28px" } }}
+        aria-label="customized table"
+      >
         <TableHead
           sx={{
-            borderRadius: '8px',
-            color: '#A19E9E',
+            borderRadius: "8px",
+            color: "#A19E9E",
             border: 0,
-            backgroundColor: '#ffffff0a',
-            backdropFilter: 'blur(25px)',
-          }}>
+            backgroundColor: "#ffffff0a",
+            backdropFilter: "blur(25px)",
+          }}
+        >
           <TableRow
             sx={{
-              '& th': {
-                padding: '8px',
+              "& th": {
+                padding: "8px",
               },
-            }}>
-            {columns.map(col => (
-              <StyledTableCell sx={{ width: col.width, maxWidth: col.width, textAlign: col.textAlign }} key={col.field}>
-                {typeof col.headerName === 'string'
-                  ? renderTypography({ sx: { color: '#A19E9E' }, text: col.headerName })
+            }}
+          >
+            {columns.map((col) => (
+              <StyledTableCell
+                sx={{
+                  width: col.width,
+                  maxWidth: col.width,
+                  textAlign: col.textAlign,
+                }}
+                key={col.field}
+              >
+                {typeof col.headerName === "string"
+                  ? renderTypography({
+                      sx: { color: "#A19E9E" },
+                      text: col.headerName,
+                    })
                   : col.headerName}
               </StyledTableCell>
             ))}
@@ -173,22 +233,22 @@ const BasicTable = <T extends Record<string, unknown>>(props: TableProps<T>) => 
               slotProps={{
                 select: {
                   inputProps: {
-                    'aria-label': 'rows per page',
+                    "aria-label": "rows per page",
                   },
                   MenuProps: {
                     slotProps: {
                       paper: {
-                        className: 'sinbad',
+                        className: "sinbad",
                         sx: {
-                          backgroundColor: 'dark.800',
-                          color: 'white',
-                          '& .MuiMenu-list': {
+                          backgroundColor: "dark.800",
+                          color: "white",
+                          "& .MuiMenu-list": {
                             padding: 0,
-                            '& .MuiTablePagination-menuItem:hover': {
-                              backgroundColor: 'dark.500',
+                            "& .MuiTablePagination-menuItem:hover": {
+                              backgroundColor: "dark.500",
                             },
-                            '& .MuiTablePagination-menuItem.Mui-selected': {
-                              backgroundColor: 'dark.500',
+                            "& .MuiTablePagination-menuItem.Mui-selected": {
+                              backgroundColor: "dark.500",
                             },
                           },
                         },
@@ -199,13 +259,15 @@ const BasicTable = <T extends Record<string, unknown>>(props: TableProps<T>) => 
               }}
               onPageChange={handleChangePage}
               onRowsPerPageChange={handleChangeRowsPerPage}
-              ActionsComponent={props => <TablePaginationActions {...props} />}
+              ActionsComponent={(props) => (
+                <TablePaginationActions {...props} />
+              )}
               sx={{
                 borderBottom: 0,
-                color: 'white',
-                fontFamily: 'var(--font-montserrat)',
-                '& .MuiTablePagination-selectIcon': {
-                  color: 'white',
+                color: "white",
+                fontFamily: "var(--font-montserrat)",
+                "& .MuiTablePagination-selectIcon": {
+                  color: "white",
                 },
               }}
             />
@@ -218,4 +280,4 @@ const BasicTable = <T extends Record<string, unknown>>(props: TableProps<T>) => 
 
 export default BasicTable;
 
-BasicTable.displayName = 'BasicTable';
+BasicTable.displayName = "BasicTablee";
