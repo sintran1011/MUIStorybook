79d3b81211d485cde00871ae43c83b4e537772e6
tam-tran-vns <tam.tran@vnsilicon.net>
2025-09-19 15:59:16 +0700

    test: deployed

--- src/stories/Checkbox/index.tsx
+++ src/stories/Checkbox/index.tsx
@@ -1,12 +1,9 @@
-import React from "react";
-import { Checkbox, useTheme } from "@mui/material";
+import React from 'react';
+import { Checkbox, Stack, useTheme } from '@mui/material';
 
 export interface BasicCheckboxProps {
   checked?: boolean;
-  onChange?: (
-    event: React.ChangeEvent<HTMLInputElement>,
-    checked: boolean,
-  ) => void;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => void;
   indeterminate?: boolean;
   disabled?: boolean;
   value?: string;
@@ -14,40 +11,35 @@ export interface BasicCheckboxProps {
 }
 
 const BasicCheckbox = (props: BasicCheckboxProps) => {
-  const {
-    checked,
-    value,
-    onChange,
-    disabled = false,
-    indeterminate = false,
-    style,
-  } = props;
+  const { checked, value, onChange, disabled = false, indeterminate = false, style } = props;
   const { palette } = useTheme();
 
   return (
-    <Checkbox
-      indeterminate={indeterminate}
-      checked={checked}
-      onChange={onChange}
-      disabled={disabled}
-      sx={{
-        "& .MuiSvgIcon-root": {
-          color: palette.border.secondary,
-        },
-        "&.Mui-checked .MuiSvgIcon-root": {
-          color: palette.brand.main,
-        },
-        "&.Mui-disabled": {
-          opacity: "0.5",
-        },
-      }}
-      color="secondary"
-      value={value}
-      style={style}
-    />
+    <Stack>
+      <Checkbox
+        indeterminate={indeterminate}
+        checked={checked}
+        onChange={onChange}
+        disabled={disabled}
+        sx={{
+          '& .MuiSvgIcon-root': {
+            color: palette.border.secondary,
+          },
+          '&.Mui-checked .MuiSvgIcon-root': {
+            color: palette.brand.main,
+          },
+          '&.Mui-disabled': {
+            opacity: '0.5',
+          },
+        }}
+        color="secondary"
+        value={value}
+        style={style}
+      />
+    </Stack>
   );
 };
 
 export default BasicCheckbox;
 
-BasicCheckbox.displayName = "BasicCheckbox";
+BasicCheckbox.displayName = 'BasicCheckbox';
